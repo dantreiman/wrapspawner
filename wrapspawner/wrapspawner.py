9190fc73d621c4a9832c461e580a68966cba1856
Daniel Treiman <dtreiman@alivecor.com>
2018-02-02 09:51:10 -0800

    Copy oauth_client_id and server to child spawner.  https://github.com/jupyterhub/wrapspawner/issues/10

--- wrapspawner/wrapspawner.py
+++ wrapspawner/wrapspawner.py
@@ -73,6 +73,8 @@ class WrapSpawner(Spawner):
                 hub  = self.hub,
                 authenticator = self.authenticator,
                 config = self.config,
+                oauth_client_id = self.oauth_client_id,
+                server = self._server,
                 **self.child_config
                 )
             # initial state will always be wrong since it will see *our* state
@@ -208,7 +210,8 @@ class ProfilesSpawner(WrapSpawner):
         try:
             self.child_profile = state['profile']
         except KeyError:
-            raise KeyError('jupyterhub database might be outdated, please reset it, in the default configuration, just delete jupyterhub.sqlite')
+            self.child_profile = ''
+            #raise KeyError('jupyterhub database might be outdated, please reset it, in the default configuration, just delete jupyterhub.sqlite')
         self.select_profile(self.child_profile)
 
     def get_state(self):
